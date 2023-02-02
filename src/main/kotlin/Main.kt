e1ddc6ffd8ba85abdfeed168a12319e4877e9186
Lucky-Wangari <wangarimarylucky>
2023-02-02 09:55:45 +0300

    lucky

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var fullnames = "Lucky Wangari"
+    var age = 24
+    var telephone = "0743712959"
+    var weightInKgs = 49
+    var isKenyan: Boolean = true
+    var nonKenyan: Boolean = false
+
+    println(isKenyan)
+
+}
+
+
